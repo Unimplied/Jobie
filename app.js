12549f1006a5850c0922437070a00883a3a5d358
Unimplied <Doomboy212@gmail.com>
2021-01-18 18:23:58 -0500

    added basic styling to index, bootstrap 5

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 
+const ejsMate = require('ejs-mate');
 const path = require('path');
 const Application = require('./models/application');
 
@@ -20,6 +21,7 @@ db.once("open", () => {
 
 const app = express();
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
