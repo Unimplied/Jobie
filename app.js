5d95a43f16b031afdbec34695f8b9efd6dca9570
Unimplied <Doomboy212@gmail.com>
2021-01-26 21:36:31 -0500

    fixed database link

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const users = require('./routes/users');
 const ejsMate = require('ejs-mate');
 const path = require('path');
 
-const dbUrl = 'mongodb://localhost:27017/Jobie';
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/Jobie';
 // process.env.DB_URL || Don't forget to add this to dbUrl
 
 const storeSecret = process.env.STORE_SECRET || 'thisismylocaldevsessionsecret';
